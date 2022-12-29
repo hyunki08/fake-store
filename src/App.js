20ede5379829681978f96d3084b5d2b2a600d447
hyunki <hyunki08@gmail.com>
2022-12-29 15:32:21 +0900

    tailwind 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1 className="text-5xl font-bold">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
