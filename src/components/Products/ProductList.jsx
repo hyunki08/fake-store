35b6818041eed3132a22bdc49da80a0a27631a77
hyunki <hyunki08@gmail.com>
2022-12-30 13:52:48 +0900

    스켈레톤 처리 추가

--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -17,10 +17,11 @@ const ProductList = () => {
 
   return (
     <div className="grid w-full auto-rows-[1fr] grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-4">
-      {products.length > 0 &&
-        products.map((product) => (
-          <ProductListItem key={product.id} product={product} />
-        ))}
+      {products.length > 0
+        ? products.map((product) => (
+            <ProductListItem key={product.id} product={product} />
+          ))
+        : Array.from({ length: 8 }).map((_, i) => <ProductListItem key={i} />)}
     </div>
   );
 };
--- src/components/Products/ProductListItem.jsx
+++ src/components/Products/ProductListItem.jsx
@@ -1,27 +1,42 @@
 const ProductListItem = ({ product }) => {
   return (
-    <div className="shadow-gray group flex min-h-[400px] cursor-pointer flex-col overflow-hidden rounded-b-3xl shadow-lg transition-all hover:bg-[rgb(54,97,235)] hover:shadow-2xl">
-      <div className="flex h-2/3 justify-center overflow-hidden bg-white">
-        <img
-          className="object-contains max-h-[300px] min-h-[200px] scale-90 transition-all group-hover:scale-95"
-          src={product.image}
-          alt={product.title}
-        />
-      </div>
-      <div className="mx-3 mt-3 flex h-1/3 flex-col gap-[2px]">
-        <div className="p h-3/5">
-          <div className="font-bold text-[rgb(34,34,34)] transition-all line-clamp-2 group-hover:text-[rgb(240,240,240)] md:text-lg">
-            {product.title}
+    <>
+      {!!product ? (
+        <div className="shadow-gray group flex min-h-[400px] cursor-pointer flex-col overflow-hidden rounded-b-3xl shadow-lg transition-all hover:bg-[rgb(54,97,235)] hover:shadow-2xl">
+          <div className="flex h-2/3 justify-center overflow-hidden bg-white">
+            <img
+              className="object-contains max-h-[250px] scale-90 transition-all group-hover:scale-95"
+              src={product.image}
+              alt={product.title}
+            />
           </div>
-          <div className="text-xs text-[rgb(180,180,180)] transition-all line-clamp-1 group-hover:text-[rgb(230,230,230)] md:text-sm">
-            {product.category}
+          <div className="mx-3 mt-3 flex h-1/3 flex-col gap-[2px]">
+            <div className="h-3/5">
+              <div className="font-bold text-[rgb(34,34,34)] transition-all line-clamp-2 group-hover:text-[rgb(240,240,240)] md:text-lg">
+                {product.title}
+              </div>
+              <div className="text-xs text-[rgb(180,180,180)] transition-all line-clamp-1 group-hover:text-[rgb(230,230,230)] md:text-sm">
+                {product.category}
+              </div>
+            </div>
+            <div className="mt-1 h-2/5 font-price tracking-wider text-[rgb(34,34,34)] transition-all group-hover:text-[rgb(240,240,240)] md:text-xl">
+              ${product.price}
+            </div>
           </div>
         </div>
-        <div className="mt-1 h-2/5 font-price tracking-wider text-[rgb(34,34,34)] transition-all group-hover:text-[rgb(240,240,240)] md:text-xl">
-          ${product.price}
+      ) : (
+        <div className="shadow-gray min-h-[400px] rounded-b-3xl shadow-lg">
+          <div className="flex h-2/3 max-h-[250px] animate-pulse justify-center overflow-hidden bg-slate-100"></div>
+          <div className="mx-3 mt-3 flex h-1/3 flex-col gap-[2px]">
+            <div className="flex h-1/2 flex-col gap-1">
+              <div className="h-7 animate-pulse rounded-full bg-slate-100"></div>
+              <div className="h-5 animate-pulse rounded-full bg-slate-100"></div>
+            </div>
+            <div className="h-8 animate-pulse rounded-full bg-slate-100"></div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
