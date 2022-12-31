b6744671161513c1aef7038427bc57a7b370d5e2
hyunki <hyunki08@gmail.com>
2022-12-31 18:27:57 +0900

    main 구현

---
+++ src/components/Layouts/Main.jsx
@@ -0,0 +1,9 @@
+const Main = ({ children }) => {
+  return (
+    <main className="mx-auto flex w-full max-w-[1100px] justify-center px-[2%] pt-2 pb-20">
+      {children}
+    </main>
+  );
+};
+
+export default Main;
