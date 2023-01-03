3e86321c8fcdf56f68100f2939273ae9c1f430de
hyunki <hyunki08@gmail.com>
2023-01-03 18:49:33 +0900

    모바일 메뉴 구현

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,16 +1,27 @@
-import { Menu, ShoppingCart } from "@mui/icons-material";
+import { Menu, Close, ShoppingCart } from "@mui/icons-material";
 import { NavMenu } from "../../constants/Nav";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Logo from "./../common/Logo";
 import { useSessionStorage } from "../../hooks/useSessionStorage";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const [user, setUser] = useSessionStorage("user", {});
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
 
-  const hadleSignOut = () => {
+  const onClickSignOut = () => {
     alert("You have been logged out.");
     setUser({});
   };
+
+  const onClickMobileMenu = () => {
+    setOpen(!open);
+  };
+
+  useEffect(() => {
+    setOpen(false);
+  }, [location.pathname]);
 
   return (
     <header className="sticky top-0 z-10 w-full bg-white md:top-[-36px]">
@@ -29,15 +40,12 @@ const Header = () => {
             className={`cursor-pointer p-2 text-sm transition-all hover:text-[rgb(100,100,100)] ${
               Object.keys(user).length > 0 ? "" : "hidden"
             }`}
-            onClick={hadleSignOut}
+            onClick={onClickSignOut}
           >
             Sign Out
           </div>
         </div>
         <nav className="mx-auto flex h-20 w-full max-w-[1100px] justify-between px-[2%] py-6 md:h-24">
-          {/* <Link to={"/"} className="w-[64px] cursor-pointer px-2">
-            <img src={logo} alt="" className="h-full object-contain" />
-          </Link> */}
           <Logo className="w-[64px] px-2" />
           <div className="hidden md:flex md:w-[calc(100%-252px)] md:justify-center md:gap-2">
             {NavMenu.map((v) => (
@@ -58,8 +66,31 @@ const Header = () => {
               <ShoppingCart />
             </Link>
             <div className="w-[32px] cursor-pointer rounded-full p-1 hover:bg-[rgb(54,97,235)] hover:text-[rgb(240,240,240)] md:hidden md:w-[48px] md:p-3">
-              <Menu className="mb-2" />
+              {open ? (
+                <Close className="mb-2" onClick={onClickMobileMenu} />
+              ) : (
+                <Menu className="mb-2" onClick={onClickMobileMenu} />
+              )}
             </div>
+            {open && (
+              <div className="fixed top-[80px] left-0 right-0 flex h-[calc(100vh-80px)] flex-col gap-6 overflow-hidden bg-white px-4 py-10">
+                {NavMenu.map((v) => (
+                  <Link
+                    to={v.path}
+                    className="cursor-pointer rounded-full p-3 transition-all hover:bg-[rgb(54,97,235)] hover:text-[rgb(240,240,240)]"
+                    key={v.path}
+                  >
+                    {v.name}
+                  </Link>
+                ))}
+                <Link
+                  className="mt-8 cursor-pointer rounded-full p-3 transition-all hover:bg-[rgb(54,97,235)] hover:text-[rgb(240,240,240)]"
+                  to={"/signin"}
+                >
+                  Sign In
+                </Link>
+              </div>
+            )}
           </div>
         </nav>
       </div>
