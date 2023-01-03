aaf3d4e31fd64e6ee21be014705958cdc0496458
hyunki <hyunki08@gmail.com>
2023-01-03 09:37:37 +0900

    로컬 스토리지, 타이틀 설정 커스텀 훅 구현

---
+++ src/hooks/useTitle.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+const updateTitle = (title) => {
+  const htmlTitle = document.querySelector("title");
+  htmlTitle.innerText = title;
+};
+
+export const useTitle = (iniitialTitle) => {
+  const [title, setTitle] = useState(iniitialTitle);
+
+  useEffect(() => {
+    updateTitle(title);
+  }, [title]);
+
+  return setTitle;
+};
+
+export const useSetTitle = (title) => {
+  useEffect(() => {
+    updateTitle(title);
+  }, [title]);
+};
